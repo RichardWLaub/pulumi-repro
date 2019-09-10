abd63cd78f3e696cda967beb9e9439b5fe085fea
Richard Laub <richard.laub@mindbodyonline.com>
2019-09-10 12:01:09 -0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as k8s from '@pulumi/kubernetes';
+
+const istioInit = new k8s.helm.v2.Chart('istio-init', {
+  path: 'istio-1.1.4/install/kubernetes/helm/istio-init'
+});
+
+new k8s.helm.v2.Chart(
+  'istio',
+  {
+    path: 'istio-1.1.4/install/kubernetes/helm/istio',
+    values: {
+      gateways: { enabled: false },
+      galley: { enabled: false },
+      mixer: { policy: { enabled: false }, telemetry: { enabled: false } },
+      pilot: { autoscaleEnabled: true, autoscaleMax: 5, autoscaleMin: 2 },
+      prometheus: { enabled: false },
+      sidecarInjectorWebhook: { enabled: false }
+    }
+  },
+  { dependsOn: [istioInit] }
+);
